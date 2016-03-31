42d0e98da67018a1074750a006e4afcfe4ddf37b
Ben Parnell <benjaminparnell.94@gmail.com>
2016-03-31 20:40:32 +0100

    Add more text and picture

--- js/index.js
+++ js/index.js
@@ -4,9 +4,10 @@ window.jQuery = $
 require('../bower_components/typed.js')
 
 $('.title > a').typed({
-  strings: [ "Hi.^700 I'm @benjaminparnell.",
+  strings: [ 'Hi.^700 I\'m @benjaminparnell.',
              '^700 I like open source things.^700',
              'And typing.^700 Typing a lot.^700',
+             'I make things with <span class="node">Node.js</span>,^200 <span class="elixir">Elixir</span> ^200and <span class="ruby">Ruby</span>.^700',
              '@benjaminparnell'
            ],
   typeSpeed: 50
